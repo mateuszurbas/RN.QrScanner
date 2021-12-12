02ac4b291a7f77990114b991f7dc86b8aa443c4d
Mateusz Urbas <mateusz.urbas@monterail.com>
2021-12-12 23:14:29 +0100

    Adds Start Screen

--- App.tsx
+++ App.tsx
@@ -1,44 +1,12 @@
 import { useFonts, Roboto_500Medium } from '@expo-google-fonts/roboto';
-import { Ionicons } from '@expo/vector-icons';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
 
-import { Button, ButtonVariants } from '@/components/Button';
+import { StartScreen } from '@/screens/StartScreen';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_500Medium,
   });
 
-  return fontsLoaded ? (
-    <View style={styles.container}>
-      <Button
-        label="Zeskanuj"
-        icon={<Ionicons name="md-barcode" size={19} color="white" />}
-        onPress={() => {
-          console.log('Zeskanuj');
-        }}
-        style={{ marginBottom: 40 }}
-      />
-
-      <Button
-        label="Twoje dane"
-        variant={ButtonVariants.secondary}
-        onPress={() => {
-          console.error("Action can't be done");
-        }}
-      />
-      <StatusBar style="auto" />
-    </View>
-  ) : null;
+  return fontsLoaded ? <StartScreen /> : null;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/StartScreen/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+import { Colors } from '@/theme/colors';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  separator: {
+    marginBottom: 40,
+  },
+});
+
+export default styles;
