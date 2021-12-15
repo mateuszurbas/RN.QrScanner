8f9e6aaff7a7a1e7aee7aacf096b4a4e1a7fd80b
Mateusz Urbas <mateusz.urbas@monterail.com>
2021-12-15 02:27:58 +0100

    jest setup, react-navigation

--- jest-setup.js
+++ jest-setup.js
@@ -1,1 +1,13 @@
+/* eslint-disable no-undef */
 require('react-native-reanimated/lib/reanimated2/jestUtils').setUpTests();
+
+jest.mock('@react-navigation/native', () => {
+  const actualNav = jest.requireActual('@react-navigation/native');
+  return {
+    ...actualNav,
+    useFocusEffect: () => jest.fn(),
+    useNavigation: () => ({
+      navigate: jest.fn(),
+    }),
+  };
+});
